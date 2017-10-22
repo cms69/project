ab8128d8aeb0a73e1313c8d6bd6dd810d434f4d9
Chaitanya M Shah <cms69@afsaccess1.njit.edu>
2017-10-22 17:51:31 -0400

    added index file

--- index.php
+++ index.php
@@ -15,3 +15,19 @@ include $class . '.php';
 spl_autoload_register(array('Manage', 'autoload'));
 
 $obj = new main();
+class main {
+public function __construct() {
+$pageRequest = 'homepage';
+if (isset($_REQUEST['page'])) {
+$pageRequest = $_REQUEST['page'];
+}
+$page = new $pageRequest;
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+$page->get();
+} else {
+$page->post();
+}
+}
+}
+
+
