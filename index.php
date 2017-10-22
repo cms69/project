ad1813319d6bec2e385e55db81f0db9f81825a5e
Chaitanya M Shah <cms69@afsaccess1.njit.edu>
2017-10-22 18:12:22 -0400

    added index file

--- index.php
+++ index.php
@@ -2,17 +2,12 @@
 <?php
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
-
 class Manage {
-
 public static function autoload($class) {
-
 include $class . '.php';
-	    }
-
+}
 }
 spl_autoload_register(array('Manage', 'autoload'));
-
 $obj = new main();
 class main {
 public function __construct() {
